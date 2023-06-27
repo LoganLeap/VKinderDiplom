fa40e9426b9e0ff40c0dfc6fee8cc888367f1fc8
LoganLeap <136262463+LoganLeap@users.noreply.github.com>
2023-06-27 23:36:23 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-version_api = 5.131
-access_token_group = 'vk1.a.r-c1MQ30194i6vGlJLVNwu541-jy-HnJ_BiePIrYtajOyhZ_Q8Zm_ljYUBcG3-VjaY-Mj9ofVXNtRuFwMNR_sZ9f0KAUZctn5qipFW3e_1IyVzFpLj9nLXG__3_JEBUZeyuDH987KyU5DWEeqvUClVttaUaeW90VEi1EhQv3Q5Ze1DHPMapFVdViyQO3C-iyy9wpp0XXSb_XsZIAjNZjcQ'
-group_id = 'https://vk.com/club221074180'
-user_token = 'vk1.a.t5BU5wc6CGK1lI9h_LsmRgBjw5b-NlvyeYU1LI7cGNkyo9jEfANremabFq1yYyye1VIqnyRbQDj_HNV9d0G18mCErK0WG1YMeXO_ca8_xpEa6jx2ENZAKVHFfWkiuiNkHqPon_WVh8tRynzfekUJyBQVPJFlB672CQ1z0ZSKJGSHjB0Lhzp_QLVDgRCadjPIwJKCkO-QnGO0jXRTFrTgRA'
-vk_id_app = '51683815'
-bd_user = 'postgres'
-bd_password = 'qazwe'
-bd_database = 'VKinder'
-host = 'localhost'
